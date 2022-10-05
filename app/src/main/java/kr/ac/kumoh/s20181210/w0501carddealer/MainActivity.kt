7a3979d348aaff8e2478ef2163485dccdb5cbe88
choijungwo <cjw2332@naver.com>
2022-10-05 15:29:38 +0900

    스앱 5주차 carddealer

--- app/src/main/java/kr/ac/kumoh/s20181210/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181210/w0501carddealer/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.ac.kumoh.s20181210.w0501carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20181210.w0501carddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,36 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+        //binding.card1.setImageResource(R.drawable.c_10_of_clubs)
+        val c = Random.nextInt(52)
+        Log.i("Test","$c : ${getCardName(c)}")
+
+        val res = resources.getIdentifier(
+            getCardName(c),
+            "drawable",
+            packageName
+        )
+
+        binding.card1.setImageResource(res)
     }
+}
+
+private fun getCardName(c: Int) : String{
+    val shape = when(c/13) {
+        0 -> "spades"
+        1 -> "diamonds"
+        2 -> "hearts"
+        3 -> "clubs"
+        else -> "error"
+    }
+
+    val number = when (c % 13) {
+        0 -> "ace"
+        in 1..9 -> (c % 13 +1).toString()
+        10 -> "jack"
+        11 -> "queen"
+        12 -> "king"
+        else -> "error"
+    }
+    return "c_${number}_of_${shape}"
 }
