ca41af9db2c8e924d3e9b963c3bceac9dfdd62b0
choijungwo <cjw2332@naver.com>
2022-10-12 14:59:05 +0900

    🎉 스앱 6주차 카드딜러

--- app/src/main/java/kr/ac/kumoh/s20181210/w0601cardviewmodel/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181210/w0601cardviewmodel/MainActivity.kt
@@ -3,68 +3,59 @@ package kr.ac.kumoh.s20181210.w0601cardviewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import kr.ac.kumoh.s20181210.w0601cardviewmodel.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private val model: CardViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        //binding.card1.setImageResource(R.drawable.c_10_of_clubs)
-        /*val c = Random.nextInt(52)
-        Log.i("Card","$c : ${getCardName(c)}")
+        model.cards.observe(this, Observer<IntArray> {
+            val res = IntArray(5)
+            for (i in it.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        })
 
-        val res = resources.getIdentifier(
-            getCardName(c),
-            "drawable",
-            packageName
-        )
-        binding.card1.setImageResource(res)*/
-        val c = IntArray(5)
-        val res = IntArray(5)
+        binding.btnDeal.setOnClickListener {
+            model.deal()
+        }
+    }
 
-        for(i in c.indices){
-            c[i] = Random.nextInt(52)
-
-            Log.i("Test", "${c[i]} : " + "${getCardName(c[i])}")
-
-            res[i] = resources.getIdentifier(
-                getCardName(c[i]),
-                "drawble",
-                packageName
-            )
+    private fun getCardName(c: Int) : String {
+        Log.i("Card", c.toString())
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
         }
 
-        binding.card1.setImageResource(res[0])
-        binding.card1.setImageResource(res[1])
-        binding.card1.setImageResource(res[2])
-        binding.card1.setImageResource(res[3])
-        binding.card1.setImageResource(res[4])
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
-}
-
-private fun getCardName(c: Int) : String{
-    val shape = when(c/13) {
-        0 -> "spades"
-        1 -> "diamonds"
-        2 -> "hearts"
-        3 -> "clubs"
-        else -> "error"
-    }
-
-    val number = when (c % 13) {
-        0 -> "ace"
-        in 1..9 -> (c % 13 +1).toString()
-        10 -> "jack"
-        11 -> "queen"
-        12 -> "king"
-        else -> "error"
-    }
-    return "c_${number}_of_${shape}"
 }
